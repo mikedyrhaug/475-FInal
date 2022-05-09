abf4e5fea3a4beb16d1a39642637ed2fbcf2e6c9
mikedyrhaug <dyrhaugm@gmail.com>
2022-05-09 12:52:27 -0400

    Added dropdown choice

--- ui.R
+++ ui.R
@@ -3,13 +3,20 @@ titlePanel("Welcome to my shiny app! To use features, click below")
 
 ui <- fluidPage(
           titlePanel("Welcome to my shiny app! To use features, click below"),
-          radioButtons("radio", label = h3("Choose a graph to show Mike Trout Search Stastistics"),
-               choices = c("General Search Trend" , "Classical Decomposition", 
-                           "Seasonality", 'Autocorrelation', 'Forecast'), 
-               selected = "General Search Trend"),
+          selectInput("radio", label = h3("Select which model you would like to see"), 
+                      choices = list("General Search Trend" , "Classical Decomposition", "Seasonality", 'Autocorrelation', 'Forecast'), 
+                      selected = "General Search Trend"),
+          
+          
+      #   radioButtons("radio", label = h3("Choose a graph to show Mike Trout Search Stastistics"),
+      #        choices = c("General Search Trend" , "Classical Decomposition", 
+      #                    "Seasonality", 'Autocorrelation', 'Forecast'), 
+      #        selected = "General Search Trend"),
   hr(),
   plotOutput('Plot'),
   textOutput("text"),
-  titlePanel("")
+  fluidRow(column(3, verbatimTextOutput("value")))
+  
+  #titlePanel("")
 )
 
